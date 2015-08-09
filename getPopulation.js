a84e18b799facf00c7dc436da77b02194316a025
Luba Amiraslanova <yunnii@yandex-team.ru>
2015-08-10 01:40:29 +0500

    Промежуточный рабочий вариант с сохраненим континента

--- getPopulation.js
+++ getPopulation.js
@@ -1,25 +1,116 @@
-module.exports = function getPopulation(continentName, countriesInfo, citiesInfo, populationInfo) {
-    var countriesInContinent = countriesInfo.reduce(function(res, country) {
-        if (country.continent === continentName) {
-            res[country.name] = country.name;
-        }
+var populationIndex = function (countries, cities, populations) {
+    this.Cache = {
+        cities: {},
+        countries: {},
+        continents: {}
+    };
 
-        return res;
-    }, {});
+    this.countries = countries;
+    this.cities = cities;
+    this.populations = populations;
+};
 
-    var citiesInCountry = citiesInfo.reduce(function(res, city) {
-        if(countriesInContinent.hasOwnProperty(city.country)) {
-            res[city.name] = city.name;
-        }
+populationIndex.prototype.update = function (countries, cities, populations) {
+    this.Cache = {
+        cities: {},
+        countries: {},
+        continents: {}
+    };
 
-        return res;
-    }, {});
+    this.countries = countries;
+    this.cities = cities;
+    this.populations = populations;
+};
 
-    return populationInfo.reduce(function(count, population) {
-        if(citiesInCountry.hasOwnProperty(population.name)) {
+/**
+ * Численность населения городов
+ *
+ * @private
+ * @param cities
+ * @returns {*}
+ * @private
+ */
+populationIndex.prototype._getPopulationByCities = function (cities) {
+    return this.populations.reduce(function(count, population) {
+        if(cities.hasOwnProperty(population.name)) {
             count += population.count;
         }
 
         return count;
     }, 0);
 };
+
+/**
+ * Численность населения города
+ *
+ * @param city
+ * @returns {*}
+ */
+populationIndex.prototype.getPopulationByCity = function(city) {
+    var query = {};
+    query[city] = city;
+
+    return this._getPopulationByCities(query);
+};
+
+/**
+ * @private
+ *
+ * Численность населения списка стран
+ *
+ * @param countries
+ * @returns {*}
+ * @private
+ */
+populationIndex.prototype._getPopulationByCountries = function(countries) {
+    var citiesInCountry = this.cities.reduce(function(res, city) {
+        if(countries.hasOwnProperty(city.country)) {
+            res[city.name] = city.name;
+        }
+
+        return res;
+    }, {});
+
+    return this._getPopulationByCities(citiesInCountry);
+};
+
+/**
+ * Численность населения страны
+ *
+ * @param country
+ * @returns {*}
+ */
+populationIndex.prototype.getPopulationByCountry = function(country) {
+    var query = {};
+    query[country] = country;
+
+    return this._getPopulationByCountries(country);
+};
+
+/**
+ * Численность населения на континенте
+ *
+ * @param continent
+ * @returns {*}
+ */
+populationIndex.prototype.getPopulationByContinent = function(continent) {
+
+    if (!this.Cache.continents[continent]) {
+        var countriesInContinent = this.countries.reduce(function(res, country) {
+            if (country.continent === continent) {
+                res[country.name] = country.name;
+            }
+
+            return res;
+        }, {});
+
+        this.Cache.continents[continent] = {
+            countries: countriesInContinent,
+            count: this._getPopulationByCountries(countriesInContinent)
+        };
+    }
+
+    return this.Cache.continents[continent].count;
+};
+
+module.exports = populationIndex;
--- shri.js
+++ shri.js
@@ -1,15 +1,17 @@
 var Promise = require('bluebird');
 
-var getPopulation = require('./getPopulation');
+var populationIndex = require('./getPopulation');
 var getInfo = require('./getInfo');
+
+var population;
 
 Promise.all([
     getInfo('/countries'),
     getInfo('/cities'),
     getInfo('/populations')
-]).spread(getPopulation.bind(null, 'Africa'))
-    .then(function (count) {
-        console.log('Total population in African cities: ' + count);
+]).spread(function(countries, cities, populations) {
+        population = new populationIndex(countries, cities, populations);
+        console.log(population.getPopulationByContinent('Africa'));
     })
     .error(function (err) {
         console.error(err);
